28df030066e0b1a7730e459420a9e89a11670a3d
yarafan <yara91@mail.ru>
2015-10-26 22:34:27 +0300

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   attr_accessor :password, :password_confirmation
   before_save :encrypt
 
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, format: { with: /[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+\.[a-zA-Z]+{2,4}/, message: 'has invalid format' }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
   def encrypt
     salt = self.pass_salt = BCrypt::Engine.generate_salt
     self.pass_hash = BCrypt::Engine.hash_secret(password, salt) if password
