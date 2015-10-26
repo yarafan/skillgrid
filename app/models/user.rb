d7968d1599ad7d2ff38df3d6f2f5e69118113a1f
yarafan <yara91@mail.ru>
2015-10-26 21:31:08 +0300

    add user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessor :password, :password_confirmation
+  before_save :encrypt
+
+  def encrypt
+    salt = self.pass_salt = BCrypt::Engine.generate_salt
+    self.pass_hash = BCrypt::Engine.hash_secret(password, salt) if password
+  end
 end
