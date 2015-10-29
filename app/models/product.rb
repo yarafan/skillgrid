906804f03fe1ae2fb65f2efc5bb5eccf53cad6e6
yarafan <yara91@mail.ru>
2015-10-29 21:42:35 +0300

    add presence validation for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { thumb: '100x100', large: '500x500' }
   validates_attachment_content_type :image, content_type: /\Aimage/
+  validates :title, presence: true
+  validates :description, presence: true
 end
