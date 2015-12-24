fc6435763d8600634dec7b657870ec148ba65e21
yarafan <yara91@mail.ru>
2015-12-24 22:56:50 +0300

    codestyle product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,8 @@ class Product < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :image, styles: { thumb: '100x100', large: '500x500' }
-  validates_attachment_content_type :image, content_type: /\Aimage/
   validates :title, presence: true
   validates :description, presence: true
+  has_attached_file :image, styles: { thumb: '100x100', large: '500x500' }
+  validates_attachment_content_type :image, content_type: /\Aimage/
 end
